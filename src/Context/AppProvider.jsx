7b6ee4f025ba6ae25daf25575b76a1aa8590043f
LuisVasconcelos0713 <Lgvs1307@gmail.com>
2026-01-15 12:14:10 -0300

    fixing case sensitive bugs

--- src/Context/AppProvider.jsx
+++ src/Context/AppProvider.jsx
@@ -1,4 +1,4 @@
-import { AppContext } from "./Appcontext";
+import { AppContext } from "./AppContext";
 
 export const AppProvider = ({children}) => {
     const BASE_URL = "https://opensheet.elk.sh/1f3-kCM5h9FOEAjkRLJ24HdNhRdHOr7eaII7wpRz6pO0";
